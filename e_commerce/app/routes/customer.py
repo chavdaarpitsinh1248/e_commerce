ef7dbe2d696691ac3b57745c5448cf3571d8af4f
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-12-01 16:08:41 +0530

    Delete e_commerce/app directory

--- e_commerce/app/routes/customer.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-
-customer_bp = Blueprint("customer", __name__)
-
-@customer_bp.route("/status")
-def status():
-    return jsonify({"message": "Customer API working"})
--- e_commerce/app/routes/public.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-
-public_bp = Blueprint("public", __name__)
-
-@public_bp.route("/status")
-def status():
-    return jsonify({"message": "Public API working"})
