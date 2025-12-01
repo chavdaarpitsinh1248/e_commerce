6bd0468db580a0f22c202924cd881fbb4f15bad9
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-12-01 18:41:53 +0530

    Add files via upload

--- app/routes/public.py
+++ app/routes/public.py
@@ -6,3 +6,22 @@ public_bp = Blueprint("public", __name__)
 @public_bp.route("/status")
 def status():
     return jsonify({"message": "Public API working"})
+
+
+
+@public_bp.route("/products", methods=["GET"])
+def get_products():
+    products = Product.query.all()
+
+    data = []
+    for p in products:
+        data.append({
+            "id": p.id,
+            "title": p.title,
+            "price": p.price,
+            "stock": p.stock,
+            "thumbnail": p.thumbnail,
+            "category": p.category.name if p.category else None,
+        })
+
+    return jsonify(data)
