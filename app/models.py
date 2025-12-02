546449836687e40a51c8ce7db63f96387b9de64f
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-12-02 17:56:14 +0530

    Add files via upload

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from app.extensions import db, login_manager
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 # ----------------------------------------------------------------
 # LOGIN LOADER
@@ -20,7 +22,7 @@ class User(db.Model, UserMixin):
 
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
 
     role = db.Column(db.String(20), nullable=False, default="customer")
 
@@ -37,6 +39,12 @@ class User(db.Model, UserMixin):
     def is_supplier(self):
         return self.role == "supplier"
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
 
 
 # ----------------------------------------------------------------
